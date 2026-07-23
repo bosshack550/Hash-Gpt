fe4667871b0e7311fc67a6a8c88c039284552978
bosshack550 <bosshack550@gmail.com>
2026-07-24 00:06:32 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const AI_CONFIG = {
     API_KEY: "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",
 
     // OpenRouter model
-    MODEL: "google/gemma-4-31b-it:free",
+    MODEL: "poolside/laguna-s-2.1:free",
 
     // Default AI name
     NAME: "Hash GPT",
