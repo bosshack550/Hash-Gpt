c88df09f8635d351116c1cfcce5624dac55e94ba
bosshack550 <bosshack550@gmail.com>
2026-07-23 13:01:33 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const AI_CONFIG = {
+
+    // Put your API key here
+    API_KEY: "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",
+
+    // OpenRouter model
+    MODEL: "openai/gpt-oss-20b:free",
+
+    // Default AI name
+    NAME: "Hash GPT",
+
+    // Developer
+    CREATOR: "Crypt Hashir"
+
+};
