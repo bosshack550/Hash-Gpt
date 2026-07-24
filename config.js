b3fca423f8db48a5f61fd35d28add8e4485bbdf9
bosshack550 <bosshack550@gmail.com>
2026-07-24 07:12:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const AI_CONFIG = {
 
-API_KEY:"AQ.Ab8RN6KOYjV2832GtnfratLwdXzKyeWULat1-e3PDx1wObU8uw",
+API_KEY: "AQ.Ab8RN6KOYjV2832GtnfratLwdXzKyeWULat1-e3PDx1wObU8uw",
 
-MODEL:"gemini-2.5-flash",
+MODEL: "gemini-2.5-flash",
 
-NAME:"Hash GPT",
+NAME: "Hash GPT",
 
-CREATOR:"Crypt Hashir"
+CREATOR: "Crypt Hashir"
 
 };
